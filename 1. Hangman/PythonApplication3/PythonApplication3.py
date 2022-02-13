14f2939df47ff358467b538e8457b98c705a20f8
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-02-14 03:00:09 +0900

    Update PythonApplication3.py

--- 1. Hangman/PythonApplication3/PythonApplication3.py
+++ 1. Hangman/PythonApplication3/PythonApplication3.py
@@ -18,25 +18,39 @@ while True:
     s = ['_'] * len_word
     attempt = 0
     while ''.join(s) != word:
-        print('Enter the letter. %s' % ''.join(s))
+        print('Enter the letter or entire word. %s' % ''.join(s))
         letter = input().rstrip()
         exist = False
-
-        for i in range(len_word):
-            if word[i] == letter:
-                exist = True
-                s[i] = letter
-
-        if ''.join(s) == word:
-            print()
-            print('Done! The answer is %s' % word)
-            print('Your total attempt is %d' % attempt)
-            print()
-            break
-
-        if not exist:
-            print('There are no %s' % letter)
         
+        if len(letter) == len(s):
+            if letter == word:
+                print()
+                print('Done! The answer is %s' % word)
+                print('Your total attempt is %d' % attempt)
+                print()
+                break
+            else:
+                print('%s is not the answer.' % letter)
+
+        elif len(letter) != len(s) and len(letter) > 1:
+            print('Invalid input.')
+
+        else:
+            for i in range(len_word):
+                if word[i] == letter:
+                    exist = True
+                    s[i] = letter
+
+            if ''.join(s) == word:
+                print()
+                print('Done! The answer is %s' % word)
+                print('Your total attempt is %d' % attempt)
+                print()
+                break
+
+            if not exist:
+                print('There are no %s' % letter)
+            
         attempt += 1
         print('Current attempt is %d' % attempt)
         print()
